143b2ba119b4455d7c8e8245fec8b1ac31f009ed
devmallett <34248685+devmallett@users.noreply.github.com>
2021-10-29 14:40:00 -0400

    Streamlined api key

--- list.py
+++ list.py
@@ -1,38 +1,43 @@
-import requests
-import pybase64
-import json
-from secrets import GORGIAS_TICKET_ID 
-from secrets import GORGIAS_CUSTOMER_ID 
-from secrets import GORGIAS_API
-from secrets import GORGIAS_STORE_NAME
-from secrets import GORGIAS_CUSTOMER_EMAIL
+# import requests
+# import pybase64
+# import json
+# from secrets import GORGIAS_TICKET_ID 
+# from secrets import GORGIAS_CUSTOMER_ID 
+# from secrets import GORGIAS_API
+# from secrets import GORGIAS_STORE_NAME
+# from secrets import GORGIAS_CUSTOMER_EMAIL
 
 
 
-# def pull_ticket_id():
-#pass through a first name 
-#if first name matches api call of first name
-#return that first name's id number 
+# # def pull_ticket_id():
+# #pass through a first name 
+# #if first name matches api call of first name
+# #return that first name's id number 
 
-def pull_customer_id(customer_email):
-    url = f"https://{GORGIAS_STORE_NAME}.gorgias.com/api/tickets"
+# def pull_customer_id(customer_email):
+#     url = f"https://{GORGIAS_STORE_NAME}.gorgias.com/api/tickets"
 
-    querystring = {"per_page":"30","page":"1"}
+#     querystring = {"per_page":"30","page":"1"}
 
-    headers = {
-        "Accept": "application/json",
-        "Authorization": f"Basic {GORGIAS_API}"
-    }
+#     headers = {
+#         "Accept": "application/json",
+#         "Authorization": f"Basic {GORGIAS_API}"
+#     }
 
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    # print(response.text)
+#     response = requests.request("GET", url, headers=headers, params=querystring)
+#     # print(response.text)
 
-    data = json.loads(response.text)
-    for id in data['data']:
-        if id['requester']['email'] == customer_email:
-            print(id['requester']['id'])
+#     data = json.loads(response.text)
+#     for id in data['data']:
+#         if id['requester']['email'] == customer_email:
+#             print(id['requester']['id'])
 
-pull_customer_id('dev@lifebydam.com')
+# pull_customer_id('dev@lifebydam.com')
 
 
 
+def some_function():
+    print("hello world")
+
+def new_function():
+    some_function()
--- main.py
+++ main.py
@@ -6,30 +6,34 @@ from secrets import GORGIAS_CUSTOMER_ID
 from secrets import GORGIAS_API
 from secrets import GORGIAS_STORE_NAME
 from secrets import GORGIAS_CUSTOMER_EMAIL
+from secrets import GORGIAS_ACCOUNT_EMAIL
 
 
 
 
 class SendMessage:
-    def __init__(self ,ticket_id ,customer_id ,customer_name ,api_key,store_name ,customer_email ,ticket_message): 
+    def __init__(self ,ticket_id ,customer_name ,store_name ,customer_email ,ticket_message): 
         self.ticket_id = ticket_id
-        self.customer_id = customer_id
+        self.customer_id = ""
         self.customer_name = customer_name
         self.customer_email = customer_email
-        self.api_key = api_key
         self.store_name = store_name
         self.ticket_message = ticket_message
         self.header = {}
         
+        self.to_be_encoded = f'{GORGIAS_ACCOUNT_EMAIL}:{GORGIAS_API}'
+        
+        self.encoded_api = pybase64.b64encode(str.encode(self.to_be_encoded), altchars='_:').decode('utf-8')
         
     def pull_customer_id(self):
+        print(self.encoded_api, "This is encoded api")
         url = f"https://{GORGIAS_STORE_NAME}.gorgias.com/api/tickets"
 
         querystring = {"per_page":"30","page":"1"}
 
         headers = {
             "Accept": "application/json",
-            "Authorization": f"Basic {GORGIAS_API}"
+            "Authorization": f"Basic {self.encoded_api}"
         }
 
         response = requests.request("GET", url, headers=headers, params=querystring)
@@ -38,12 +42,13 @@ class SendMessage:
         data = json.loads(response.text)
         for id in data['data']:
             if id['requester']['email'] == self.customer_email:
-                print(id['requester']['id'])
+                self.customer_id = id['requester']['id']
+                # print(self.customer_id)
     
     
     def make_request(self):
         # pull_customer_id()
-        
+        # print(self.customer_id, "this is cutomer id")
         url = f"https://{self.store_name}.gorgias.com/api/tickets/{self.ticket_id}/messages"
         
         payload = {
@@ -73,15 +78,16 @@ class SendMessage:
         self.header = {
             "Accept": "application/json",
             "Content-Type": "application/json",
-            "Authorization": f"Basic {self.api_key}"
+            "Authorization": f"Basic {self.encoded_api}"
         }
         response = requests.request("POST", url, json=payload, headers=self.header)
         print(response.text)
 
 
 
-new_message = SendMessage(GORGIAS_TICKET_ID ,GORGIAS_CUSTOMER_ID , "Dmalzzzz" ,GORGIAS_API ,GORGIAS_STORE_NAME ,GORGIAS_CUSTOMER_EMAIL ,'This is with encoded api call')
-# new_message.make_request()
+new_message = SendMessage(GORGIAS_TICKET_ID ,"Dmalzzzz" ,GORGIAS_STORE_NAME ,GORGIAS_CUSTOMER_EMAIL ,'Encoded in script')
 new_message.pull_customer_id()
+new_message.make_request()
+
         
         
