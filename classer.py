746859780601929db5f8205f001d9cd99dc3e6d5
devmallett <34248685+devmallett@users.noreply.github.com>
2021-10-29 10:31:16 -0400

    Customer id pulled

--- classer.py
+++ classer.py
@@ -1,5 +1,6 @@
 import requests
 import pybase64
+import json
 from secrets import GORGIAS_TICKET_ID 
 from secrets import GORGIAS_CUSTOMER_ID 
 from secrets import GORGIAS_API
@@ -19,6 +20,25 @@ class SendMessage:
         self.header = {}
         
         
+    def pull_customer_id(self):
+        url = f"https://{GORGIAS_STORE_NAME}.gorgias.com/api/tickets"
+
+        querystring = {"per_page":"30","page":"1"}
+
+        headers = {
+            "Accept": "application/json",
+            "Authorization": f"Basic {GORGIAS_API}"
+        }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+        # print(response.text)
+
+        data = json.loads(response.text)
+        for id in data['data']:
+            if id['requester']['email'] == self.customer_email:
+                print(id['requester']['id'])
+    
+    
     def make_request(self):
         
         url = f"https://{self.store_name}.gorgias.com/api/tickets/{self.ticket_id}/messages"
@@ -61,7 +81,8 @@ class SendMessage:
         # print(self.ticket_message)
 
 
-new_message = SendMessage(GORGIAS_TICKET_ID ,GORGIAS_CUSTOMER_ID , "Dmalzzzz" ,GORGIAS_API ,GORGIAS_STORE_NAME ,GORGIAS_CUSTOMER_EMAIL ,'This is a class call')
-new_message.make_request()
+new_message = SendMessage(GORGIAS_TICKET_ID ,GORGIAS_CUSTOMER_ID , "Dmalzzzz" ,GORGIAS_API ,GORGIAS_STORE_NAME ,GORGIAS_CUSTOMER_EMAIL ,'This is with encoded api call')
+# new_message.make_request()
+new_message.pull_customer_id()
         
         
---
+++ list.py
@@ -0,0 +1,38 @@
+import requests
+import pybase64
+import json
+from secrets import GORGIAS_TICKET_ID 
+from secrets import GORGIAS_CUSTOMER_ID 
+from secrets import GORGIAS_API
+from secrets import GORGIAS_STORE_NAME
+from secrets import GORGIAS_CUSTOMER_EMAIL
+
+
+
+# def pull_ticket_id():
+#pass through a first name 
+#if first name matches api call of first name
+#return that first name's id number 
+
+def pull_customer_id(customer_email):
+    url = f"https://{GORGIAS_STORE_NAME}.gorgias.com/api/tickets"
+
+    querystring = {"per_page":"30","page":"1"}
+
+    headers = {
+        "Accept": "application/json",
+        "Authorization": f"Basic {GORGIAS_API}"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    # print(response.text)
+
+    data = json.loads(response.text)
+    for id in data['data']:
+        if id['requester']['email'] == customer_email:
+            print(id['requester']['id'])
+
+pull_customer_id('dev@lifebydam.com')
+
+
+
